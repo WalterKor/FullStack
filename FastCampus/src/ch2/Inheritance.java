989fdb67ad2bb50701cc8d8fed259f75b251a278
okchanho <j33515@naver.com>
2021-04-11 22:47:12 +0900

    4월 11일

---
+++ FastCampus/src/ch2/Inheritance.java
@@ -0,0 +1,29 @@
+package ch2;
+
+public class Inheritance {
+/*
+  상속은 언제 사용할까?
+   	 일반적개념과 구체적인 개념의 관계
+ 	 상위클래스 : 하위클래스보다 일반적인 개념 ex)Employee
+ 	 하위클래스 : 상위클래스보다 구체적인 개념들이 더해짐 ex)Engineer
+ 	 상속은 클래스간의 결합도가 높은 설계
+ 	 상위 클래스의 수정이 많은 하위 클래스에 영향을 미칠 수 있다. 
+ 	 계층이 복잡하거나 
+ 	 
+ 	 <다운 캐스팅과 instance of>
+ 	 업캐스팅된 클래스를 다시 원래의 타입으로 형 변환할때 
+ 	 하위 클래스로의 형변환을 명시적으로 해야할때
+ 	 
+ 	 customer vc = new VIPCustomer(); //묵시적
+ 	 VIPCustomer vCustomer = (VIPCustomer)vc; //명시적 
+ 	 
+ 	 instanceof : 참조변수가 그 객체의 참조변수인지 아닌지 확인하는것
+ 	 원래 인스턴스의 형이 맞는지 여부를 체크하는 키워드
+ 	 맞으면 true 아니면 false를 반환한다.
+ 	 
+ 	  
+ 	 
+ 	 
+ 	 
+ */
+}
