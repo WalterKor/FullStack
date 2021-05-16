20c733001a502458db2aa297339052bb2081d0c8
everglow <78077569+okchanho@users.noreply.github.com>
2021-05-16 23:27:50 +0900

    Update Calculator.java

--- FastCampus/src/Interface/Calculator.java
+++ FastCampus/src/Interface/Calculator.java
@@ -5,7 +5,7 @@ public abstract class Calculator implements Calc {
 	@Override
 	public int add(int num1, int num2) {//모든 메서드를 구현해라
 		return (num1 + num2);
-	}
+	} 
 
 	@Override
 	public int substract(int num1, int num2) {
